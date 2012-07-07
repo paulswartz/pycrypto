8d7f6632782e934ec3474b9953a8e0b9a735c8bc
Paul Swartz <paulswartz@gmail.com>
2012-07-07 14:19:27 -0400

    add untested code to generate ECDSA keys

--- lib/Crypto/PublicKey/_ECDSA.py
+++ lib/Crypto/PublicKey/_ECDSA.py
@@ -27,89 +27,35 @@
 
 __revision__ = "$Id$"
 
-from Crypto.PublicKey.pubkey import *
+import math
+
 from Crypto.Util import number
-from Crypto.Util.number import bytes_to_long, long_to_bytes
-from Crypto.Hash import SHA
-from Crypto.Util.py3compat import *
 
 
 class error (Exception):
     pass
 
 
-def generateQ(randfunc):
-    S=randfunc(20)
-    hash1=SHA.new(S).digest()
-    hash2=SHA.new(long_to_bytes(bytes_to_long(S)+1)).digest()
-    q = bignum(0)
-    for i in range(0,20):
-        c=bord(hash1[i])^bord(hash2[i])
-        if i==0:
-            c=c | 128
-        if i==19:
-            c= c | 1
-        q=q*256+c
-    while (not isPrime(q)):
-        q=q+2
-    if pow(2,159L) < q < pow(2,160L):
-        return S, q
-    raise RuntimeError('Bad q value generated')
+def generate_py(curve, randfunc, progress_func=None):
+    """generate(curve:CurveDomain, randfunc:callable, progress_func:callable)
 
-def generate_py(domain, randfunc, progress_func=None):
-    """generate(domain:CurveDomain, randfunc:callable, progress_func:callable)
-
-    Generate a ECDSA key from domain 'domain', using 'randfunc' to get random
+    Generate a ECDSA key on curve 'curve', using 'randfunc' to get random
     data and 'progress_func', if present, to display the progress of the key
     generation.
     """
-    if not domain.valid():
-        raise ValueError('Invalid domain')
-    obj=ECDSAobj()
-    # Generate string S and prime q
+    if not curve.verify():
+        raise ValueError('Invalid curve')
+    obj = ECDSAobj()
+    obj.curve = curve
+    # Generate private key d and public key Q = dg
     if progress_func:
-        progress_func('p,q\n')
-    while (1):
-        S, obj.q = generateQ(randfunc)
-        n=divmod(bits-1, 160)[0]
-        C, N, V = 0, 2, {}
-        b=(obj.q >> 5) & 15
-        powb=pow(bignum(2), b)
-        powL1=pow(bignum(2), bits-1)
-        while C<4096:
-            for k in range(0, n+1):
-                V[k]=bytes_to_long(SHA.new(S+bstr(N)+bstr(k)).digest())
-            W=V[n] % powb
-            for k in range(n-1, -1, -1):
-                W=(W<<160L)+V[k]
-            X=W+powL1
-            p=X-(X%(2*obj.q)-1)
-            if powL1<=p and isPrime(p):
-                break
-            C, N = C+1, N+n+1
-        if C<4096:
-            break
-        if progress_func:
-            progress_func('4096 multiples failed\n')
-
-    obj.p = p
-    power=divmod(p-1, obj.q)[0]
+        progress_func('d\n')
+    obj.d = number.getRandomRange(1, curve.n - 1, randfunc)
     if progress_func:
-        progress_func('h,g\n')
-    while (1):
-        h=bytes_to_long(randfunc(bits)) % (p-1)
-        g=pow(h, power, p)
-        if 1<h<p-1 and g>1:
-            break
-    obj.g=g
-    if progress_func:
-        progress_func('x,y\n')
-    while (1):
-        x=bytes_to_long(randfunc(20))
-        if 0 < x < obj.q:
-            break
-    obj.x, obj.y = x, pow(g, x, p)
+        progress_func('Q\n')
+    obj.Q = curve.G * obj.d
     return obj
+
 
 class ECDSAobj:
     pass
@@ -248,7 +194,7 @@ class PrimeCurveDomain(CurveDomain):
                     return False
 
         # verify P is prime
-        if not isPrime(self.p):
+        if not number.isPrime(self.p):
             print 'p'
             return False
 
@@ -272,7 +218,7 @@ class PrimeCurveDomain(CurveDomain):
             return False
 
         # verify N is prime
-        if not isPrime(self.n):
+        if not number.isPrime(self.n):
             return False
 
         return True
